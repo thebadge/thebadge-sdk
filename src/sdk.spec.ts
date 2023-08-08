9aab35177b5b60f98a970cc1c2f5cf71b7b5127b
Fernando Ramirez <fernando.ramirez@diva-e.com>
2023-08-09 01:09:32 +0200

    Add more tests

--- src/sdk.spec.ts
+++ src/sdk.spec.ts
@@ -2,6 +2,9 @@ import { RPCProvider, TheBadgeSDK, TheBadgeSDKPermissions } from './index'
 import { Web3Provider } from '@ethersproject/providers'
 
 describe('TheBadgeSDK', () => {
+  const infuraProvider = { name: RPCProvider.infura, token: '' }
+  const alchemyProvider = { name: RPCProvider.alchemy, token: '' }
+
   it('should check if a chain id is supported or not', () => {
     expect(TheBadgeSDK.isChainSupported(5)).toBeTruthy() // goerli is supported
     expect(TheBadgeSDK.isChainSupported(100)).toBeTruthy() // gnosis is supported
@@ -9,10 +12,7 @@ describe('TheBadgeSDK', () => {
     expect(TheBadgeSDK.isChainSupported(56)).toBeFalsy() // bnb is not supported
   })
 
-  it('should check if TheBadgeSDK instance is initialized correctly', () => {
-    const infuraProvider = { name: RPCProvider.infura, token: '' }
-    const alchemyProvider = { name: RPCProvider.alchemy, token: '' }
-
+  it('should initialize a TheBadgeSDK instance correctly', () => {
     const sdk1 = new TheBadgeSDK(5, { rpcProviderConfig: infuraProvider })
     expect(sdk1.getChainId()).toBe(5)
     expect(sdk1.getRPCProviderName()).toBe(RPCProvider.infura)
@@ -38,5 +38,21 @@ describe('TheBadgeSDK', () => {
     expect(sdk3.isDevMode()).toBeTruthy()
     expect(sdk3.getWeb3Provider()).not.toBeUndefined()
     expect(sdk3.getPermissions()).toBe(TheBadgeSDKPermissions.READ_AND_WRITE)
+  })
+
+  it('should initialize the needed services correctly', () => {
+    const sdk = new TheBadgeSDK(5, { rpcProviderConfig: infuraProvider })
+
+    // badges service
+    expect(sdk.badges).not.toBeUndefined()
+    expect(sdk.badges.get).not.toBeUndefined()
+
+    // badge models service
+    expect(sdk.badgeModels).not.toBeUndefined()
+    expect(sdk.badgeModels.get).not.toBeUndefined()
+
+    // users service
+    expect(sdk.users).not.toBeUndefined()
+    expect(sdk.users.getIds).not.toBeUndefined()
   })
 })

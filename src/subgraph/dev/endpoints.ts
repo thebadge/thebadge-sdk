8609d2ab5f3a131141c14a2464070c0a2f38a529
apane <agustin.pane@gmail.com>
2023-10-16 09:37:01 +0200

    Update graphs urls

--- src/subgraph/dev/endpoints.ts
+++ src/subgraph/dev/endpoints.ts
@@ -1,7 +1,7 @@
 // subgraph URLs
 
 // Goerli
-const GOERLI_SG_URL_DEV = 'https://api.thegraph.com/subgraphs/name/thebadgeadmin/thebadge-goerli-dev'
+const GOERLI_SG_URL_DEV = 'https://api.studio.thegraph.com/query/51391/thebadge-goerli-dev/version/latest'
 
 // Sepolia
 const SEPOLIA_SG_URL_DEV = 'https://api.studio.thegraph.com/query/51391/thebadge-sepolia-dev/version/latest'

e5aacf069acb20f5859e74f008cff1b91569940a
Agustin <agustin.pane@gmail.com>
2023-10-18 10:29:02 +0200

    Feat: Update sc addresses (#26)

--- src/contracts/contracts.ts
+++ src/contracts/contracts.ts
@@ -6,8 +6,8 @@ export const contracts = {
   TheBadge: {
     address: {
       // TODO check support of different envs per chain
-      [SupportedChains.goerli]: '0x24a2cC73D3b33fa92B9dc299835ec3715FB033fB',
-      [SupportedChains.sepolia]: '0xE60E872Bb117AC85DBf62377557023DA9BB0e45f',
+      [SupportedChains.goerli]: '0xF118b243eBBAB8166D23Eb98Ace86fEFdE62A748',
+      [SupportedChains.sepolia]: '0x70d6b6cdB3ce3FE21EefE4F967Bb2d8e12E0F701',
       // [SupportedChains.gnosis]: 'null',
     },
     abi: TheBadge,

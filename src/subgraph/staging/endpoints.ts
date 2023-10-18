5ba7829805dd14ae1c591a94d71cec987ea6e0b3
apane <agustin.pane@gmail.com>
2023-10-18 11:12:47 +0200

    Update endpoints sg

--- src/subgraph/staging/endpoints.ts
+++ src/subgraph/staging/endpoints.ts
@@ -1,10 +1,10 @@
 // subgraph URLs
 
 // Goerli
-const GOERLI_SG_URL_QA = 'https://api.studio.thegraph.com/query/51391/thebadge-goerli-staging/version/latest'
+const GOERLI_SG_URL_QA = 'https://api.studio.thegraph.com/query/51391/thebadge-goerli-staging/1.0.6'
 
 // Sepolia
-const SEPOLIA_SG_URL_QA = 'https://api.studio.thegraph.com/query/51391/thebadge-sepolia-staging/version/latest'
+const SEPOLIA_SG_URL_QA = 'https://api.studio.thegraph.com/query/51391/thebadge-sepolia-staging/1.0.6'
 
 export default {
   // chainId: {

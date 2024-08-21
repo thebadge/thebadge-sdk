2997e6aa3b5695b91a48672d81fbca3e68a03280
Agustin <agustin.pane@gmail.com>
2024-08-21 10:26:03 +0200

    Fix tests Update goerli instead of sepolia

--- src/services/badgeModels/badgeModels.spec.ts
+++ src/services/badgeModels/badgeModels.spec.ts
@@ -12,4 +12,32 @@ describe('BadgeModelsService', () => {
     expect('get' in badgeModelsService).toBeTruthy()
     expect('notExistingMethod' in badgeModelsService).toBeFalsy()
   })
+  it('should query a tp model and should return data', async () => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const badgeModelsService = new BadgeModelsService(11155111, {
+      rpcProviderConfig: { name: RPCProvider.infura, apiKey: '' },
+    })
+
+    const metadata = await badgeModelsService.getMetadataOfBadgeModel(2)
+
+    expect(metadata.badgeModelThirdPartyMetaData.id).toBe('2')
+    expect(badgeModelsService).not.toBeUndefined()
+    expect('get' in badgeModelsService).toBeTruthy()
+    expect('notExistingMethod' in badgeModelsService).toBeFalsy()
+  })
+  it('should query a kleros model and should return data', async () => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const badgeModelsService = new BadgeModelsService(11155111, {
+      rpcProviderConfig: { name: RPCProvider.infura, apiKey: '' },
+    })
+
+    const metadata = await badgeModelsService.getMetadataOfBadgeModel(12)
+
+    expect(metadata.badgeModelKlerosMetaData.id).toBe('12')
+    expect(badgeModelsService).not.toBeUndefined()
+    expect('get' in badgeModelsService).toBeTruthy()
+    expect('notExistingMethod' in badgeModelsService).toBeFalsy()
+  })
 })
